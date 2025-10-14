7a56283e8c7f7056be5bbadfc876857eb22d7261
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-14 21:50:27 +0100

    Update asset bundles and PharmacyMap.jsx
    Regenerated asset bundles to reference the updated main index bundle. Updated import paths in bundled files to use the new index asset. Modified PharmacyMap.jsx and index.html, likely to support the new build output.

--- dist/assets/MessageWithLinks-C4hF3M-j.js
+++ dist/assets/MessageWithLinks-C4hF3M-j.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-CNzO3OZO.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-ot7c0ZH8.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
