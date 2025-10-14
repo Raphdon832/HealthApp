a8ee46e806599fe5a64f5a2bb5f9326f78c65531
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-14 18:35:02 +0100

    Update build assets and onboarding page
    Regenerated build assets to use the new main bundle (index-BSL4IG5u.js) and updated references in dependent files. Also made changes to src/pages/auth/Onboarding.jsx and updated dist/index.html to reflect the new asset names.

--- dist/assets/MessageWithLinks-DngZS9-A.js
+++ dist/assets/MessageWithLinks-DngZS9-A.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-C-x1roao.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-BSL4IG5u.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
