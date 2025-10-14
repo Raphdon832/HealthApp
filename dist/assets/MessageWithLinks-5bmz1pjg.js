e64e7d8471f797023b16845dad9e9fc554386b0e
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-14 10:19:57 +0100

    Update onboarding assets and refactor build outputs
    Added onboarding images and onboarding.html for onboarding flow. Updated and replaced several build output files in dist/assets, including CSS and JS bundles, and updated references to the new bundle names. Minor update to Onboarding.jsx and index.html to support onboarding changes.

--- dist/assets/MessageWithLinks-5bmz1pjg.js
+++ dist/assets/MessageWithLinks-5bmz1pjg.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-CmC6g14v.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-C8nHrbJU.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
