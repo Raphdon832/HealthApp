f73b5b432d63d95c2d18fc4545558d84f61550f3
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-14 21:04:59 +0100

    Update asset bundles and fix SalesTrends.jsx
    Regenerated asset bundles to reference the updated main index bundle. Updated import paths in built JS files to use the new index bundle. Modified SalesTrends.jsx and index.html, likely to support new or updated features or bug fixes.

--- dist/assets/MessageWithLinks-DisqmsX7.js
+++ dist/assets/MessageWithLinks-DisqmsX7.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-cB9gPphB.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-CNzO3OZO.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
