25dbc5e588f6bd3c1f1ef668fa33a8b1eb324edb
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-14 21:52:17 +0100

    Update asset builds and add new CSS file
    Renamed and updated several JavaScript asset files to use the new index-CX-SH_F7.js bundle. Added a new CSS file (index-B34LpWCW.css) and removed the old index-CfiafNS1.css. Updated dist/index.html to reference the new assets.

--- dist/assets/MessageWithLinks-BDAMhMpX.js
+++ dist/assets/MessageWithLinks-BDAMhMpX.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-ot7c0ZH8.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-CX-SH_F7.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
