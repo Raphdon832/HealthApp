e063871b88aa801ffc8305a59925dac6699c28de
Raphdon832 <50739318+Raphdon832@users.noreply.github.com>
2025-10-18 02:50:08 +0100

    Update asset bundles and language/profile pages
    Regenerated asset bundles to reference the new main index chunk. Updated imports in CreatePrescriptionModal and MessageWithLinks to use the new index bundle. Minor changes in language and profile pages, and updated CSS asset references.

--- dist/assets/MessageWithLinks-ChBCBHDg.js
+++ dist/assets/MessageWithLinks-ChBCBHDg.js
@@ -1,4 +1,4 @@
-import{c as h,r as m,j as e,G as y}from"./index-CaNt5IfC.js";/**
+import{c as h,r as m,j as e,G as y}from"./index-BKsqKmrv.js";/**
  * @license lucide-react v0.452.0 - ISC
  *
  * This source code is licensed under the ISC license.
